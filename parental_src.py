f2f5ecf01d468311833aba40db33be346360c3dd
Sindastra <sindastra+github@gmail.com>
2018-02-05 10:13:06 +0100

    Updated copyright and license header.

--- parental_src.py
+++ parental_src.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python
-# Wii parental control password reset tool
 #
+# WiiPCRT
+# Wii Parental Control (PIN) Reset Tool – Based on https://wii.marcan.st/parental_src.py
+#
+# Copyright (C) 2018 Sindastra <https://github.com/sindastra/WiiPCRT>
 # Copyright 2008-2009 Hector Martin Cantero <hector@marcansoft.com>
 #
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 2 or version 3 of the License.
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import time, urlparse
 
